a7d86ffb7d608c3ca6bdcc8582d6ad10cd4dc5e1
vignesh <vigneshkannan1467@gmail.com>
2024-03-12 15:22:14 +0530

    Include forgotten file in the commit

--- app/rnd/page.jsx
+++ app/rnd/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeroSection from './desktop/hero-section'
-import MyComponent from './desktop/slider'
+import MyComponent from './desktop/secondary-section'
 import HeroSectionMobile from './mobile/heroSectionMobile'
 import SliderMobile from './mobile/sliderMobile'
 import Ribbon from './desktop/ribbon'
