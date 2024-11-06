2e1a62ea6b344a741231566d98e26d85c1b30e07
devdharunprasad <dharunprasaddev2002@gmail.com>
2024-11-06 09:55:17 +0530

    fix: Reimported the component

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import HeroSection from './desktop/herosection'
 import RedefiningCommunication from './desktop/redefining-communication'
 import PhoneEmailOffice from './desktop/phone-email-office'
+import HeroSection from './desktop/heroSection'
 
 const ContactUsPage = () => {
   return (
